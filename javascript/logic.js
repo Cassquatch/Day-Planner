37cf1af052588a401992e0f5ea7d04e491d963bc
Cassidy Fortner <cassidy.fortner@gmail.com>
2019-11-16 20:38:54 -0600

    trying to compare time of day to the hours on planner, to change class
    pushing for progress mainly, still working through comparing hours on planner to time of day

--- javascript/logic.js
+++ javascript/logic.js
@@ -5,17 +5,32 @@ $(document).ready(function(){
     let now = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").text(now);
 
+    //save the text entered into the textarea
+    function saveTask(){
+        let task = $("textarea").val();
+        localStorage.setItem("saved-tasks", task);
+    }
+
+    $(".saveBtn").on("click", saveTask);
     //need to compare the current hour to the hour divs on the planner, to see if the hour has passed, is the present, or has not happened yet
     let current_hour = moment().hour();
-    current_hour = moment().format("h A");
+    current_hour = moment().format("hA");
     
     console.log(current_hour);
     
     //conditionals to check the hours on the planner to the current hour,and change the background color if the hour has passed already, red if it is preset, green if it is future(consider changing to a switch statement once you figure it out)
-    if(current_hour === $(".hour").text()){
-        $("textarea").addClass("present");
-    }else if($(".hour").text() > current_hour){
-        $("textarea").addClass("past");
+    
+    let time_blocks = $(".time-block");
+    console.log(time_blocks);
+    for(let i = 0; i < time_blocks.length; i++){
+        let hour_blocks = $(time_blocks[i]);
+        let hour_blocks_text = hour_blocks.text();
+        let hour_textarea = hour_blocks.children("row").children("textarea");
+
+        if(current_hour === hour_blocks_text){
+            hour_textarea.addClass("present");
+        }
+        
     }
 
 
@@ -23,6 +38,5 @@ $(document).ready(function(){
 
 
 
-
-})
+});
 
